7309628dca82516074e47dccf6948c38bd5d992f
jameshp <michael.polt@gmail.com>
2015-07-13 18:56:30 +0200

    circles are now automatically added

--- circle_drawing_script.js
+++ circle_drawing_script.js
@@ -8,6 +8,7 @@ var entranceLines  = new L.LayerGroup();
 var exitLines = new L.LayerGroup();
 
 function Update_Layers () {
+	
 	//delete old content
 	detectionZones.clearLayers();
 	entranceLines.clearLayers(); 
@@ -23,6 +24,7 @@ function Update_Layers () {
 		gtype = features[index].geometry.type;
 		if (gtype == "Point"){
 			var radius = features[index].properties["radius"]; 
+			var id = features[index].properties["id"]; 
 			if ( isNaN(radius) ){
 				radius = default_radius;
 			}
@@ -32,6 +34,7 @@ function Update_Layers () {
 
 		//create Line layers for entrance (point 1 and point 2) and exit (point 3, and 4 of polygon)
 		if (gtype == "Polygon"){
+			var id = features[index].properties["id"]; 
 			var latlng1 = L.latLng(features[index].geometry.coordinates[0][0][1], features[index].geometry.coordinates[0][0][0]);
 			var latlng2 = L.latLng(features[index].geometry.coordinates[0][1][1], features[index].geometry.coordinates[0][1][0]);
 			L.polyline([latlng1,latlng2], {color: 'red'}).addTo(entranceLines);
@@ -71,11 +74,40 @@ map.on('draw:created', function (e) {
         layer = e.layer;
 
     console.log("my Created  hook");
-    Update_Layers();
+   
     if (type === 'marker') {
         // Do marker specific actions
     }
+    console.log("Number of features");
+    feature_length = window.api.data.get('map').features.length;
+    console.log(feature_length);
 
+    if (type === 'polygon') {
+    	//fetch last created polygon feature and set some attributes
+    	feature = window.api.data.get('map').features[feature_length-1]
+
+    	feature.properties["event"] = "EntranceExit"
+    	feature.properties["id"] = feature_length ; 
+    	feature.properties["name"] = "Zone " + feature_length;
+    	feature.properties["bothdirections"] = 0;
+
+    	//Create a Point for the created polygon
+    	console.log("Polygon was created..");
+        // Do marker specific actions
+        var coordinates = layer.getLatLngs();
+        console.log(coordinates);
+        var bounds = layer.getBounds();
+        console.log(bounds);
+        center = bounds.getCenter();
+        console.log(center);
+        //get the center of the marker and create the a marker (with radius x) automatically
+
+        //draw the marker and add it to the map 
+        window.api.data.mergeFeatures([{ type: "Feature", properties: {radius:default_radius, event:"EntranceExit", id: feature_length +1, name:"Zone " +feature_length }, geometry: { type: "Point", coordinates: [center.lng, center.lat] } }]);
+        
+    }
+
+    Update_Layers();
     // Do whatever else you need to. (save to db, add to map etc)
     //map.addLayer(layer);
 });
@@ -106,3 +138,19 @@ var config = { attributes: true, childList: true, characterData: true };
 // eigentliche Observierung starten und Zielnode und Konfiguration übergeben
 observer.observe(target, config);
 
+
+
+/*
+
+//loads a external extension of leaflet  script element
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-label/v0.2.1/leaflet.label.js';
+document.head.appendChild(script);
+
+var script = document.createElement('link');
+script.type = 'text/css';
+script.rel = 'stylesheet'
+script.href = 'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-label/v0.2.1/leaflet.label.css';
+document.head.appendChild(script);
+*/
